da2d759be12e053e8bbd41c019c071ad1ab51bb7
amarjot10 <jot57034@gmail.com>
2024-05-15 17:38:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./assets/style.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
